dbf5ddd216f7d8946a1ba4d468c5ae2e6a1737ca
UsmanSaeed787 <usmanalisaeed787@gmail.com>
2023-10-10 23:33:10 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+"use strict";
+// Inferred type: string
+let bestSong = Math.random() > 0.5
+    ? "Chain of Fools"
+    : "Respect";
+console.log(bestSong);
+// Simple Assignment operator = 
+var variableName = "Assignment Operators";
+console.log(variableName);
+var number1 = 30;
+// Addition and assignment operator
+number1 += 10; //number1 = number1 + 10
+console.log("Addition assignment Operator is =", number1);
+// subtract and assignment operator
+number1 -= 10; //number1 = number1 - 10
+console.log("Subtraction assignment Operator is =", number1);
+// multiply and assignment operator
+number1 *= 10; //number1 = number1 * 10
+console.log("Mutiplication assignment Operator is =", number1);
+// divide and assignment operator
+number1 /= 10; //number1 = number1 / 10
+console.log("Division assignment Operator", number1);
---
+++ index.ts
@@ -0,0 +1,25 @@
+// Inferred type: string
+let bestSong = Math.random() > 0.5
+? "Chain of Fools"
+: "Respect";
+console.log(bestSong)
+
+
+
+// Simple Assignment operator = 
+var variableName: string = "Assignment Operators";
+console.log(variableName)
+var number1: number = 30;
+
+// Addition and assignment operator
+number1 += 10;//number1 = number1 + 10
+console.log("Addition assignment Operator is =", number1)
+// subtract and assignment operator
+number1 -= 10;   //number1 = number1 - 10
+console.log("Subtraction assignment Operator is =",number1)
+// multiply and assignment operator
+number1 *= 10;//number1 = number1 * 10
+console.log("Mutiplication assignment Operator is =",number1)
+// divide and assignment operator
+number1 /= 10;//number1 = number1 / 10
+console.log("Division assignment Operator",number1)
